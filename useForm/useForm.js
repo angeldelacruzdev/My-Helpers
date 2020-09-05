451187c4ef53ce04432760129836c878766f10a7
Ángel De La Cruz <8610043+angeldelacruzdev@users.noreply.github.com>
2020-09-05 23:14:24 +0800

    Update useForm.js

--- useForm/useForm.js
+++ useForm/useForm.js
@@ -5,11 +5,7 @@ export const useForm = ( initialState = {} ) => {
 
     const [values, setValues] = useState( initialState );
     
-    const reset = () => {
-
-        setValues( initialState );
-
-    }
+    const reset = () => setValues( initialState );
 
     const handleInputChange = ({ target }) =>{
         setValues({
